4e6e34440e85683835c8121b476d2fe84c216243
Patrizio Bekerle <patrizio@bekerle.com>
2025-10-28 14:11:32 +0100

    playwright: pull config to new directory
    Signed-off-by: Patrizio Bekerle <patrizio@bekerle.com>

--- dbp-app/devenv.nix
+++ dbp-app/devenv.nix
@@ -4,24 +4,12 @@
   ...
 }:
 
-let
-  inherit (builtins.fromJSON (builtins.readFile "${pkgs.playwright-driver}/browsers.json")) browsers;
-  chromium-rev = (builtins.head (builtins.filter (x: x.name == "chromium") browsers)).revision;
-  firefox-rev = (builtins.head (builtins.filter (x: x.name == "firefox") browsers)).revision;
-in
 {
-  # https://devenv.sh/basics/
-  env = {
-    # Use the Playwright browsers installed by nix package for unit tests
-    CHROMIUM_BIN = lib.mkDefault "${pkgs.playwright.browsers}/chromium-${chromium-rev}/chrome-linux/chrome";
-    FIREFOX_BIN = lib.mkDefault "${pkgs.playwright.browsers}/firefox-${firefox-rev}/firefox/firefox";
-  };
-
   # https://devenv.sh/supported-languages/javascript/
   languages = {
     javascript = {
-      enable = true;
-      npm.enable = true;
+      enable = lib.mkDefault true;
+      npm.enable = lib.mkDefault true;
       # i18next-parser is not compatible with Node.js 24
       package = lib.mkDefault pkgs.nodejs_22;
     };
@@ -37,7 +25,6 @@ in
 
   enterShell = ''
     echo "📦 Node version: $(node --version | head -n 1)"
-    echo "🏁 Using Chromium at ${pkgs.chromium.version} and Firefox at ${pkgs.firefox.version} from Playwright for unit tests"
   '';
 
   # https://devenv.sh/git-hooks/
