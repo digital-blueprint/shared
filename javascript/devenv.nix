d621df42bc9d31b5c2753c4a0f2f34cab123334e
Patrizio Bekerle <patrizio@bekerle.com>
2025-11-03 14:28:21 +0100

    javascript: add shared config
    Signed-off-by: Patrizio Bekerle <patrizio@bekerle.com>

---
+++ javascript/devenv.nix
@@ -0,0 +1,31 @@
+{
+  lib,
+  ...
+}:
+let
+  inherit (lib)
+    mkDefault
+    ;
+in
+{
+  # https://devenv.sh/supported-languages/javascript/
+  languages = {
+    javascript = {
+      enable = mkDefault true;
+      npm.enable = mkDefault true;
+    };
+  };
+
+  enterShell = ''
+    echo "📦 Node version: $(node --version | head -n 1)"
+  '';
+
+  # https://devenv.sh/git-hooks/
+  git-hooks = {
+    hooks = {
+      eslint.enable = mkDefault true;
+    };
+  };
+
+  # See full reference at https://devenv.sh/reference/options/
+}
