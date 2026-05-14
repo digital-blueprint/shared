55f1d46e27a884c34b47fb7df5685b1f59a07e62
Patrizio Bekerle <patrizio@bekerle.com>
2026-05-14 14:46:06 +0200

    go: add more linters
    Signed-off-by: Patrizio Bekerle <patrizio@bekerle.com>

--- go/devenv.nix
+++ go/devenv.nix
@@ -14,6 +14,10 @@
   # https://devenv.sh/git-hooks/
   git-hooks.hooks = {
     gofmt.enable = lib.mkDefault true;
+    golangci-lint.enable = lib.mkDefault true;
+    golines.enable = lib.mkDefault true;
+    gotest.enable = lib.mkDefault true;
+    govet.enable = lib.mkDefault true;
   };
 
   # See full reference at https://devenv.sh/reference/options/
