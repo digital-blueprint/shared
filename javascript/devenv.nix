2677f653c8c19c970f7dc5743a2822b714f810d4
Christoph Reiter <reiter.christoph@gmail.com>
2026-08-27 14:33:36 +0200

    Revert "javascript: update to nodejs 25"
    This reverts commit c008abd75ea9ceff6c27a421a3782ebb5f8c61e8.
    
    it's EOL

--- javascript/devenv.nix
+++ javascript/devenv.nix
@@ -1,6 +1,5 @@
 {
   lib,
-  pkgs,
   ...
 }:
 let
@@ -14,8 +13,6 @@ in
     javascript = {
       enable = mkDefault true;
       npm.enable = mkDefault true;
-      # For npm >= 11 you need to use nodejs_25
-      package = mkDefault pkgs.nodejs_25;
     };
   };
 
